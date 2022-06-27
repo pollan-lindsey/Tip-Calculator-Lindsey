2e5def0612088224d0166471367856e4c45df198
Lindsey Pollan <lpollan@live.com>
2022-06-27 12:06:51 -0500

    morning outlining and planning

---
+++ app.js
@@ -0,0 +1,13 @@
+//set up some empty arrays
+
+//function for both buttons includes if/else statement
+//if submit button is pushed, output it into tip box/form
+//if reset button is pushed, form must be cleared (maybe i can do this in html. experimentation required)
+//need getElementById for buttons
+//math requires some methods not taught in class
+
+//for math:
+//refer to bookmark: round to 2 decimals
+
+//need local storage (JSON)
+//change array to string using JSON.stringify
