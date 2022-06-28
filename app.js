5257ee3e64c2740849b3748e075287703cb8b410
Lindsey Pollan <lpollan@live.com>
2022-06-28 10:12:43 -0500

    form1 done minus submit button

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 
 //function for both buttons includes if/else statement
 //if submit button is pushed, output it into tip box/form
-//if reset button is pushed, form must be cleared (maybe i can do this in html. experimentation required)
 //need getElementById for buttons
 //math requires some methods not taught in class
 
 //for math:
 //refer to bookmark: round to 2 decimals
+
+//for tip percentage:
 
 //need local storage (JSON)
 //change array to string using JSON.stringify
