771dd5b7bc9d9bcd999da173f5491a1f05f25d20
Lindsey Pollan <lpollan@live.com>
2022-06-30 23:38:27 -0500

    js abandoned css html under construction

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//function for submit button? includes if/else statement
+//there's an event called submit
 //if submit button is pushed, output it into tip box/form
 //need getElementById for buttons
 //math requires some methods not taught in class
@@ -23,7 +23,6 @@
 let billAmount = document.getElementById('bill-amount');
 let tipAmount = document.getElementById('tip-amount-select'); //access tip amount: tipAmount.value
 let custAmount = document.getElementById('customer-amount'); 
-let submit = document.getElementById('submit');
 let tipPerPerson = document.getElementById('tip-per-person'); 
 let totalPerPerson = document.getElementById('total-per-person');
 let submitButton = document.getElementById('submit');
@@ -41,38 +40,28 @@ let tipPercent = tipInt / 100
 //turn customer into a number
 let custInt = parseInt (custAmount, 10);
 
-
-
-//calculating tip by JS
-//Find out how to attach different percentages to the various tip amounts (do values count as children? can i refer to children in JS? (yes to both)
-//tip amount children: children[0] = 5%; children[1] = 10%; children[2] = 15%; children[3] = 25%; children[4] = 50%
-//if firstChild is used, multiply by 0.05?
-//use else if in a for loop once the above is figured out
-//event listeners to submit
-function totalBill(bill, tip, customer){
-//write math equation using bill, tip, and customer
-//bill divided by customer to get bill per customer before tip, then
+//calculate the tip
+function tipPerPerson(bill, customer){
 //calculating tip:
 //divide the bill by the number of customers: 
-let tipMath = bill / customer;
+let billMath = bill / customer;
 //multiply that by the tip as a percentage:
-
-//round it to two decimal places:
-
+let tipMath = billMath * tipPercent;
+//round it to two decimal places (Number.EPSILON makes sure the number is rounded correctly):
+let tip = Math.round((tipMath + Number.EPSILON) * 100 )/ 100;
 }
-//add event listener to total bill
+//put arguments where placeholders are in function totalBill
+//use integer values bc they are integers and therefore can be used in a math problem
+tipPerPerson(billInt, custInt);
+
+//calculate full bill
+function totalBill(tip, bill){
+    //add tip to subtotal bill
+let addedTotal = tip + bill
+}
+totalBill( )
+//add event listener to submit
 //run the numbers through math equation
 //when submit button is clicked, put all information in output boxes
+submitButton.addEventListener('click', totalBill);
 
-//using this to figure out tip amount:
-//for (i=0; i < ?; i++) {
-// if ? {
-    //totalBill * 0.05 (use math function that rounds to two decimal points)
-//}else if{
-    //?
-    //totalBill * 0.1 (use math function to round to two decimal places)
-    //do this for every tip amount and put 50% tip under 'else'
-
-//}
-//}
-//put the result of each of the above in the empty container back in html that I'll make later
